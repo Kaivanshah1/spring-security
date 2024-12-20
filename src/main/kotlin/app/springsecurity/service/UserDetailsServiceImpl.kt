475b3017247479f2f1e9800c2690df3f202d5ccf
Kaivan Shah <110343220+Kaivanshah1@users.noreply.github.com>
2024-12-20 13:51:05 +0530

    Update UserDetailsServiceImpl.kt

--- src/main/kotlin/app/springsecurity/service/UserDetailsServiceImpl.kt
+++ src/main/kotlin/app/springsecurity/service/UserDetailsServiceImpl.kt
@@ -10,10 +10,10 @@ import org.springframework.stereotype.Service
 @Service
 class UserDetailsServiceImpl(private val userRepository: UserRepository) : UserDetailsService {
 
-    override fun loadUserByUsername(username: String): UserDetails {
-        val user = userRepository.findByUsername(username).orElseThrow { UsernameNotFoundException("User not found with username $username") }
+    override fun loadUserByUsername(username: String): UserDetails {         //this function is of spring security and called during authentication to load user details by their username
+        val user = userRepository.findByUsername(username).orElseThrow { UsernameNotFoundException("User not found with username $username") }   // user is fetch from the db
 
-        return User(
+        return User(                                                                                                
             user.username,
             user.passwordHash,
             user.roles.map { it -> org.springframework.security.core.authority.SimpleGrantedAuthority("ROLE_$it")}
