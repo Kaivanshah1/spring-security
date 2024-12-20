dfc62e492527d477af07c9b5c1a07d91430e4420
Kaivan Shah <110343220+Kaivanshah1@users.noreply.github.com>
2024-12-20 13:42:08 +0530

    Update User.kt

--- src/main/kotlin/app/springsecurity/model/User.kt
+++ src/main/kotlin/app/springsecurity/model/User.kt
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "users")
-data class User(
+data class User(        //this is used to stored in db 
     @Id
     var id: String? = null,
     var username: String,
@@ -13,13 +13,13 @@ data class User(
     var refreshToken : String? = null
 )
 
-data class UserRegistrationDto(
+data class UserRegistrationDto( //this is used every time the registration request is send 
     val username: String,
     val password: String,
     val roles: List<String> = listOf("USER")
 )
 
-data class LoginRequest(
+data class LoginRequest(  //this is used when login request is send 
     val username: String,
     val password: String
 )
